48267e365efe48c611bb7aedccc0b58f74647f28
raagan-u <n0r313n7@protonmail.ch>
2024-10-10 15:09:09 +0530

    added rollup bundler & plugins for rollup

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+
+export default {
+  input: 'src/index.tsx',  // Entry point for your package
+  output: [
+    {
+      file: 'dist/index.cjs.js',  // CommonJS format
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',  // ES module format
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),  // Exclude peer dependencies like React
+    resolve(),  // Allows Rollup to resolve modules
+    commonjs(),  // Converts CommonJS modules to ES6
+    typescript({ tsconfig: './tsconfig.json' }),  // TypeScript support
+    postcss({  // Optional: For bundling CSS
+      extract: true,  // Extract CSS into separate file
+      minimize: true,  // Minify CSS
+    }),
+    terser(),  // Minify the bundle
+  ],
+  external: ['react', 'react-dom'],  // Ensure React is not bundled
+};
+
